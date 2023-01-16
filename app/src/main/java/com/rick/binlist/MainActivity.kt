6e17049303b1b1f76c9e5d1d38fe615034cb24b2
rcolejnr <ricardo.m.jnr@gmail.com>
2023-01-16 18:39:04 +0300

    packaging

--- app/src/main/java/com/rick/binlist/MainActivity.kt
+++ app/src/main/java/com/rick/binlist/MainActivity.kt
@@ -3,6 +3,7 @@ package com.rick.binlist
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -31,7 +32,12 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Column  {
+        Text(text = "Hello $name!")
+        Text(text = "Hello $name!")
+        Text(text = "Hello $name!")
+        Text(text = "Hello $name!")
+    }
 }
 
 @Preview(showBackground = true)
